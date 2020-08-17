cebf903a7ee6ecacc659933e2fd9dc34dfb1e701
lsmucassi <lindasmucassi@gmail.com>
2020-08-17 13:30:44 +0200

    theming

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,7 +21,7 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    background: #ffffff;
+    background: ${props => props.theme.background};
     overscroll-behavior: none;
     overflow-x: hidden;
   }
@@ -50,7 +50,7 @@ const Layout = ({ children }) => {
 
 
   return (
-    <ThemeProvider theme>
+    <ThemeProvider theme={darkTheme}>
       <GlobalStyle />
       <main>{children}</main>
     </ThemeProvider>
